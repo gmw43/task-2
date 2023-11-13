9081ca088d946eaf12512bb5d0104c882cf4cfb2
gmw43 <148313421+gmw43@users.noreply.github.com>
2023-11-13 17:59:47 +0500

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./component/navbar";
+import Navbar from "./component/navbar";
 import Home from "./component/home";
 import About from "./component/about";
 import Contact from "./component/contact";
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="About" element={<About />} />
           <Route path="contact" element={<Contact />} />
